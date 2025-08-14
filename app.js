64cc02632d118bcb2b357184098f0b55de68d18b
Joel Izzedin <joelizzedin.dev@gmail.com>
2025-08-14 13:57:11 -0400

    Array labs update

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ Exercise 7: Using the `slice()` method
 Complete Exercise 7 in the space below:
 */
 
-const yummy = foods.slice(1,-2)
+const yummy = foods.slice(1,3)
 
 console.log('Exercise 7 result:', yummy);
 
